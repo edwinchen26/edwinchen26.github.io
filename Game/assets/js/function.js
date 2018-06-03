e6103723cdb10bd55d0accea35a9ca4969f2d785
edwinchen26 <edwin_chen26@hotmail.com>
2018-06-03 17:37:06 -0500

    Game

---
+++ Game/assets/js/function.js
@@ -0,0 +1,21 @@
+function setAudio(audio, timeout){
+  setTimeout(function(){
+    new Audio('assets/sfx/'+audio+'.wav').play();
+  }, timeout);
+}
+
+function changeScene(from, to, timeout){
+  setTimeout(function(){
+    $("[data-scene="+from+"]").fadeOut(500, function(){ $("[data-scene="+to+"]").fadeIn(500); });
+  }, timeout);
+}
+
+/* Onload document */
+$(function(){
+  setAudio('preload', 1500);
+  changeScene('preload', 'menu', 3000);
+
+  $(".buttons > span").on('mouseup mousedown', function(){
+    $(this).toggleClass("active");
+  });
+});
