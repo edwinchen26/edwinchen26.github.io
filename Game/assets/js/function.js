4285d44ca7ac2c15ef9444333ed8e6e95b142952
edwinchen26 <edwin_chen26@hotmail.com>
2018-06-20 23:13:38 -0500

    Game update

--- Game/assets/js/function.js
+++ Game/assets/js/function.js
@@ -1,6 +1,7 @@
 // Preload SFX & BGM
 var sfx = new Howl({
   src: ['assets/sfx/intro.mp3'],
+  volume: 1,
   loop: true
 });
 
@@ -12,10 +13,15 @@ function changeScene(from, to, timeout = '500', audio){
   setTimeout(function(){
     if(typeof audio !== 'undefined'){
       $("[data-scene="+from+"]").fadeOut(500, function(){ $("[data-scene="+to+"]").fadeIn(500); });
-      //sfx.stop(); // Stop all SFX
-      //sfx.play();
+      sfx.stop(); // Stop all SFX
+      sfx.play();
     }else{
       $("[data-scene="+from+"]").fadeOut(500, function(){ $("[data-scene="+to+"]").fadeIn(500); });
+    }
+
+    if(to == "game"){
+      var game = new Game();
+      game.generate();
     }
   }, timeout);
 }
@@ -23,12 +29,8 @@ function changeScene(from, to, timeout = '500', audio){
 /* Onload document */
 $(function(){
   // Init preload
-  //setTimeout(function(){ preload.play() }, 1500);
+  setTimeout(function(){ preload.play() }, 1500);
   changeScene('preload', 'menu', 3000, 'intro');
-
-  $(".buttons > span").on('mouseup mousedown', function(){
-    $(this).toggleClass("active");
-  });
 
   // Mute sound
   var playing = true;
--- Game/assets/js/game.js
+++ Game/assets/js/game.js
@@ -1,67 +1,89 @@
-// Variables
-
-
 // Game
 class Game{
   constructor(level){
-
     var $this = this;
     this.level = level; // current level
     $this.points = 0; // Puntaje
-    $this.powerUp = 0;
     $this.status = false; // Init false ---- True -> no quita puntos cuando finaliza el timer ~ False -> quita puntos cuando finaliza el timer
     $this.clickable = true; // Init true --- True-> Permite interactuar con los botones ~ False -> Bloquea interracción con los botones
+    $this.timer = 0; // Timer en segundos
 
     $this.color = this.generate(true); // Genera un color
-    /* Detecta clic y verifica si el color es igual */
+
+    /* Keypress -> click */
+    $(document).on('keydown', function(e){
+      switch(e.keyCode){
+        case 81:
+          $("[data-color=red]").click();
+          break;
+        case 87:
+          $("[data-color=blue]").click();
+          break;
+        case 69:
+          $("[data-color=green]").click();
+          break;
+        case 82:
+          $("[data-color=yellow]").click();
+          break;
+      }
+    });
+
+    /* Detecta CLIC y verifica si el color es igual */
     $("[data-color]").on('click', function(){
       var color = $(this).data("color");
       if($this.color === color && $this.clickable === true){
-        $this.points += 1;
+        $this.points += 500;
         $this.status = true; // Estado true - evitar quitar punto por no contestar
         $this.clickable = false; // Estado false - Permite clic solo una vez
       }else if($this.color != color & $this.clickable === true){
         $this.status = true; // Estado true - evita quitar punto por no contestar
         $this.clickable = false; // Estado false - Permite clic solo una vez
-        ($this.points <= 0) ? $this.points = 0 : $this.points -= 1;
+        ($this.points <= 0) ? $this.points = 0 : $this.points -= 300;
       }
-      $(".game .world span#color").html("Color: "); // ---> TEST: Quita el texto del color anterior
-      $(".game .world span#score").html("Points = " + $this.points); // Texto -1 punto
+      //$(".game .world span#color").html("Color: "); // ---> TEST: Quita el texto del color anterior
+      $("[data-color]").removeClass("active"); // Quita 'active' de los botones
+      $(".game .world span#score").html($this.points); // Texto -1 punto
     });
 
     // Función para iniciar a generar los colores
-    var interval = Math.floor(Math.random() * (3000 - 2000) + 2000);
-    setInterval(function(){
-      interval = Math.floor(Math.random() * (3000 - 2000) + 2000);
+    var interval = Math.floor((Math.random() * 500) + 1000);
+    var blocks = setInterval(function(){
+      interval = Math.floor((Math.random() * 500) + 1000);
       $this.color = $this.generate($this.status);
-      $(".game .world span#color").html("Color: " + $this.color);
+      //$(".game .world span#color").html("Color: " + $this.color);
+      $("[data-color="+$this.color+"]").addClass("active"); // Agrega 'active' al color actual
     }, interval / 2);
 
-  }
-
-  // PowerUp
-  powerUps(currentPoints){
-    if( ((currentPoints % 5) == 0) && currentPoints >  this.powerUp){
-      this.powerUp = currentPoints;
-      console.log("POWER UP ~ " + currentPoints);
-    }
+    // Timer -> +10pts cada .1s
+    var timer = setInterval(function(){
+      ($this.points <= 0) ? $this.points = 0 : $this.points += 10;
+      $this.timer += 100;
+      if($this.timer >= 60000){
+        //onsole.log("JUEGO TERMINADO");
+        //console.log("Puntaje: " + $this.points);
+        alert("Puntuación: " + $this.points);
+        clearInterval(timer);
+        clearInterval(blocks);
+        $("[data-color]").removeClass("active"); // Quita 'active' de los botones
+        changeScene('game', 'menu');
+      }
+      $(".game .world span#score").html($this.points); // Texto -1 punto
+    }, 100);
   }
 
   // Generar los colores
   generate(status){
     var colors = ['red', 'blue', 'green', 'yellow'];
-    var rand = Math.floor(Math.random() * (colors.length - 1));
+    var rand = Math.floor(Math.random() * colors.length);
     if(status === false){ // Si no se contestó - quita puntos
-      (this.points <= 0) ? this.points = 0 : this.points -= 1;
-      $(".game .world span#score").html("Points = " + this.points); // Texto -1 punto
+      (this.points <= 0) ? this.points = 0 : this.points -= 300;
+      //$(".game .world span#color").html("Color: "); // ---> TEST: Quita el texto del color anterior
+      $("[data-color]").removeClass("active"); // Quita 'active' de los botones
+      $(".game .world span#score").html(this.points); // Texto -1 punto
     }
     this.status = false;
     this.clickable = true; // Permite interactuar con los botones nuevamente
-    this.powerUps(this.points);
     return colors[rand];
   }
 
 }
-
-var game = new Game();
-game.generate();
